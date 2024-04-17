a1d9c7351b9117380729d376f9d08dbc4bf3ca1e
Jose Germán Martínez <jgermanmm@gmail.com>
2024-04-16 21:05:22 -0400

    Agrega dependencia de sequelize en package.json Actualiza el puerto en el archivo .env Agrega configuración de conexión a la base de datos en index.js Actualiza el modelo de usuario en user.models.js

--- back/index.js
+++ back/index.js
@@ -3,11 +3,28 @@
 require("dotenv").config();
 
 const app = require("./src/app/app");
+const { conn } = require("./src/database/conexion-sequelize");
 
 const port = process.env.PORT || 8080;
 
+async function testDatabase() {
+  try {
+    await conn.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
 
-app.listen(port, () => {
-    console.log("inventario application up on port", port);
+testDatabase();
+
+conn.sync({force: false}).then(() => {
+  app.listen(port, () => {
+    console.log("Server up", port);
   });
+  }).catch((err) => {
+    console.log("error al sincronizar la base de datos", err);
+  });
+
+
   
--- back/src/models/user.models.js
+++ back/src/models/user.models.js
@@ -1,92 +1,132 @@
 const { db } = require("../database/conexion");
 
+const { Datatypes } = require("sequelize");
+
 const bcrypt = require("bcrypt");
 
-module.exports = {
-    listarUser(){
-
-        return new Promise((resolve, reject) => {
-            // Consulta SQL para obtener la lista de usuarios
-            const sql = "SELECT * FROM usuario";
-            db.query(sql,(err,results)=>{
-                if(err) reject(err);
-                else resolve(results);
-            });
-        })
-
-
-
-
+module.exports = (sequelize) => {
+  sequelize.define("Usuario", {
+    user_id : {
+      primaryKey: true,
+      type: Datatypes.INTEGER,
+      autoincrement: true,
+      allowNull: false,
     },
+    campus_id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+    },
+    rol_id: {
+      type: Datatypes.INTEGER,
+      allowNull: false,
+    },
+    username: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    user_state: {
+      type: Datatypes.BIGINT(11),
+      allowNull: false,
+    },
+  });
+};
 
- async crearUser(username, email, campus_id, rol_id, user_state, password){
-        
 
-return new Promise((resolve, reject) => {
-    
 
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+  listarUser() {
+    return new Promise((resolve, reject) => {
+      // Consulta SQL para obtener la lista de usuarios
+      const sql = "SELECT * FROM usuario";
+      db.query(sql, (err, results) => {
+        if (err) reject(err);
+        else resolve(results);
+      });
+    });
+  },
+
+  async crearUser(username, email, campus_id, rol_id, user_state, password) {
+    return new Promise((resolve, reject) => {
       // Verificar si campus_id existe en la tabla sede
       const checkCampusQuery =
-      "SELECT COUNT(*) AS count FROM sede WHERE campus_id = ?";
-    const [campusCheckResult] =  db.query(checkCampusQuery, [campus_id]);
+        "SELECT COUNT(*) AS count FROM sede WHERE campus_id = ?";
+      const [campusCheckResult] = db.query(checkCampusQuery, [campus_id]);
 
-    if (campusCheckResult[0].count === 0) {
-      return res.status(400).json({
-        status: 400,
-        error: "El campus_id proporcionado no existe en la tabla sede",
-      });
-    }
+      if (campusCheckResult[0].count === 0) {
+        return res.status(400).json({
+          status: 400,
+          error: "El campus_id proporcionado no existe en la tabla sede",
+        });
+      }
 
-    // Iniciar una transacción
-    db.beginTransaction();
+      // Iniciar una transacción
+      db.beginTransaction();
 
-    try {
-      const salt = bcrypt.genSaltSync();
+      try {
+        const salt = bcrypt.genSaltSync();
 
-      const passWordEncripted = bcrypt.hashSync(password, salt);
+        const passWordEncripted = bcrypt.hashSync(password, salt);
 
-      // Consulta SQL para insertar un nuevo usuario
-      const insertUserQuery =
-        "INSERT INTO usuario (username, email, rol_id, user_state, password, campus_id) VALUES (?, ?, ?, ?, ?, ?)";
-      const userCreateData = [
-        username,
-        email,
-        rol_id,
-        user_state,
-        passWordEncripted,
-        campus_id,
-      ];
+        // Consulta SQL para insertar un nuevo usuario
+        const insertUserQuery =
+          "INSERT INTO usuario (username, email, rol_id, user_state, password, campus_id) VALUES (?, ?, ?, ?, ?, ?)";
+        const userCreateData = [
+          username,
+          email,
+          rol_id,
+          user_state,
+          passWordEncripted,
+          campus_id,
+        ];
 
-      // Ejecutar la consulta con los valores proporcionados
-      const [resultDB] = db.query(insertUserQuery, userCreateData);
+        // Ejecutar la consulta con los valores proporcionados
+        const [resultDB] = db.query(insertUserQuery, userCreateData);
 
-      // Confirmar la transacción
-       db.commit();
+        // Confirmar la transacción
+        db.commit();
 
-      res.status(200).json({
-        status: 200,
-        data: {
-          message: "Usuario agregado con éxito",
-          user_id: resultDB.insertId,
-        },
-      });
-    } catch (error) {
-      // Revertir la transacción en caso de error
-     db.rollback();
+        res.status(200).json({
+          status: 200,
+          data: {
+            message: "Usuario agregado con éxito",
+            user_id: resultDB.insertId,
+          },
+        });
+      } catch (error) {
+        // Revertir la transacción en caso de error
+        db.rollback();
 
-      console.error(error);
-      res.status(500).json({
-        status: 500,
-        error: "Error interno del servidor",
-      });
-    }
+        console.error(error);
+        res.status(500).json({
+          status: 500,
+          error: "Error interno del servidor",
+        });
+      }
 
-    (err,results)=>{
-        if(err) reject(err);
+      (err, results) => {
+        if (err) reject(err);
         else resolve(results);
-    };
-
-})
-
-    }
+      };
+    });
+  },
 };
